8204f31abeea4a9734736e3a9361cc981a628f9f
iasinDev <iasin.dev@gmail.com>
2023-01-15 18:25:11 +0100

    update versions

--- main.go
+++ main.go
@@ -7,12 +7,13 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	body := `<h1>Golang webapp running in a Docker container for the EDEM DevSecOps course</h1>`
+	body := `<h1>Golang webapp running in a Docker container for the EDEM DevSecOps course</h1> <br> Now, rest a bit <br> <iframe src="https://chromedino.com/" frameborder="0" scrolling="no" width="100%" height="100%" loading="lazy"></iframe>
+	<style type="text/css">iframe { position: absolute; width: 100%; height: 100%; z-index: 999; }</style>`
 	fmt.Fprintf(w, body)
 }
 
 func main() {
-	log.Print("Hello world webapp started in EDEM 2023.")
+	log.Print("Hello world webapp started.")
 
 	http.HandleFunc("/", handler)
 
